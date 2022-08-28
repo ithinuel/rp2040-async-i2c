1607fbabd555151db755226986d787449411a545
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2022-08-28 22:56:38 +0100

    --wip-- [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -41,22 +41,17 @@ pub(crate) use sealed::SubSystemReset;
 const TX_FIFO_SIZE: u8 = 16;
 const RX_FIFO_SIZE: u8 = 16;
 
-pub struct AsyncI2C<'a, I2C, Pins> {
+pub struct AsyncI2C<I2C, Pins> {
     i2c: I2C,
     pins: Pins,
     waker_setter: Option<fn(core::task::Waker)>,
-    _clock: &'a rp2040_hal::clocks::SystemClock,
 }
 
-impl<T: Deref<Target = Block>, PINS> embedded_hal_async::i2c::ErrorType for AsyncI2C<'_, T, PINS> {
+impl<T: Deref<Target = Block>, PINS> embedded_hal_async::i2c::ErrorType for AsyncI2C<T, PINS> {
     type Error = Error;
 }
-impl<
-        'a,
-        T: SubSystemReset + Deref<Target = Block>,
-        Sda: PinId + BankPinId,
-        Scl: PinId + BankPinId,
-    > AsyncI2C<'a, T, (Pin<Sda, FunctionI2C>, Pin<Scl, FunctionI2C>)>
+impl<T: SubSystemReset + Deref<Target = Block>, Sda: PinId + BankPinId, Scl: PinId + BankPinId>
+    AsyncI2C<T, (Pin<Sda, FunctionI2C>, Pin<Scl, FunctionI2C>)>
 {
     /// Configures the I2C peripheral to work in controller mode
     pub fn new(
@@ -65,7 +60,7 @@ impl<
         scl_pin: Pin<Scl, FunctionI2C>,
         freq: HertzU32,
         resets: &mut RESETS,
-        system_clock: &'a rp2040_hal::clocks::SystemClock,
+        system_clock: &rp2040_hal::clocks::SystemClock,
     ) -> Self
     where
         Sda: SdaPin<T>,
@@ -148,11 +143,10 @@ impl<
             i2c,
             pins: (sda_pin, scl_pin),
             waker_setter: None,
-            _clock: system_clock,
         }
     }
 }
-impl<T, Sda, Scl> AsyncI2C<'_, T, (Pin<Sda, FunctionI2C>, Pin<Scl, FunctionI2C>)>
+impl<T, Sda, Scl> AsyncI2C<T, (Pin<Sda, FunctionI2C>, Pin<Scl, FunctionI2C>)>
 where
     T: SubSystemReset + Deref<Target = Block>,
     Sda: PinId + BankPinId,
@@ -167,7 +161,7 @@ where
     }
 }
 
-impl<'clk, T: Deref<Target = Block>, PINS> AsyncI2C<'clk, T, PINS> {
+impl<T: Deref<Target = Block>, PINS> AsyncI2C<T, PINS> {
     async fn block_on<F: FnMut(&mut Self) -> Poll<U>, U>(&mut self, mut f: F) -> U {
         // if a waker is set enbale interrupt
         futures::future::poll_fn(|cx| {
@@ -429,7 +423,7 @@ impl<'clk, T: Deref<Target = Block>, PINS> AsyncI2C<'clk, T, PINS> {
         Ok(())
     }
 
-    pub fn write_iter<'a: 'clk, A, U>(
+    pub fn write_iter<'a, A, U>(
         &'a mut self,
         address: A,
         bytes: U,
@@ -448,7 +442,7 @@ impl<'clk, T: Deref<Target = Block>, PINS> AsyncI2C<'clk, T, PINS> {
         }
     }
 }
-impl<T, PINS, A> embedded_hal_async::i2c::I2c<A> for AsyncI2C<'_, T, PINS>
+impl<T, PINS, A> embedded_hal_async::i2c::I2c<A> for AsyncI2C<T, PINS>
 where
     T: Deref<Target = Block>,
     A: embedded_hal_async::i2c::AddressMode + 'static + Into<u16>,
