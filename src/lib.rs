21bab021c9755e3fecb49b87f32ec4eee2cf9a9b
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2023-11-09 21:32:44 +0000

    Bump to new rp2040-hal & embedded-hal 1.0-rc.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 //! Implementation of the embedded-hal-async i2c traits for the Raspberry Pi's RP2040 chip.
 #![no_std]
 #![forbid(missing_docs)]
-#![allow(incomplete_features)]
-#![feature(async_fn_in_trait)]
 
 /// Implementation for the Synopsys I2C peripheral as implemented on the RP2040 chip.
 pub mod i2c;
